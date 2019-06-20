1b021d866c8b087bf4eb690a3b474a1c8bef8cd2
Pedro Correia <peviniciusac@gmail.com>
2019-06-20 20:58:14 -0300

    Criado tabela image

---
+++ VisualRecognition/VisualRecognition.Infrastructure/Migrations/20190620234019_VisualRecognition.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisualRecognition.Infrastructure.Migrations
+{
+    public partial class VisualRecognition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ImagePath = table.Column<string>(nullable: true),
+                    ImageName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ImageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+        }
+    }
+}
